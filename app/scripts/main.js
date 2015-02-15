e09cc8ff8d0027ec880374d4c621f06fb2845dab
Stéphane Vasseur <stephanevasseur@MacBook-Pro-de-Stephane-Vasseur.local>
2015-02-15 15:47:51 -0500

    Generate web app builder

---
+++ app/scripts/main.js
@@ -0,0 +1,22 @@
+console.log('\'Allo \'Allo!');
+
+// Full list of configuration options available at:
+// https://github.com/hakimel/reveal.js#configuration
+Reveal.initialize({
+	controls: true,
+	progress: true,
+	history: true,
+	center: true,
+
+	transition: 'slide', // none/fade/slide/convex/concave/zoom
+
+	// Optional reveal.js plugins
+	dependencies: [
+		// { src: 'classlist/classList.min.js', condition: function() { return !document.body.classList; } },
+		// { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+		// { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+		// { src: 'plugin/highlight/highlight.js', async: true, condition: function() { return !!document.querySelector( 'pre code' ); }, callback: function() { hljs.initHighlightingOnLoad(); } },
+		// { src: 'plugin/zoom-js/zoom.js', async: true },
+		// { src: 'plugin/notes/notes.js', async: true }
+	]
+});
